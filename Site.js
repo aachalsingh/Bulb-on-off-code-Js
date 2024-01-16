6b1eb9f5f3a1eb776ab02c6b3dcb54982ecf59a5
Aachal Singh <aachal.singh07@gmail.com>
2024-01-16 17:14:11 +0530

    first js

---
+++ Site.js
@@ -0,0 +1,12 @@
+        function bulbOn()
+        {
+            var on = document.getElementById("img")
+            on.src="light-bulb-on.jpg";       //overriding the off image
+        }
+
+        function bulbOff()
+        {
+            var on = document.getElementById("img")
+            on.src="light-bulb-off.png";       //overriding the off image
+        }
+ 
